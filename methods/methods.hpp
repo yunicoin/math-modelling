dca504aa9c320503b9883b3f7755204707d05152
yunicoin <90954972+yunicoin@users.noreply.github.com>
2026-06-08 17:07:48 +0300

    Update methods.hpp

--- methods/methods.hpp
+++ methods/methods.hpp
@@ -13,7 +13,8 @@
 namespace mm {
 
 /* Сюда нужно вставить объявление серверной части алгоритма. */
-
+int HeatEquationMethod(const nlohmann::json& input,
+                       nlohmann::json* output);
 
 /* Конец вставки. */
 
