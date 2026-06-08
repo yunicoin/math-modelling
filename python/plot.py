f4a7520dfb10f6f7251768c7aa1c4a2b61c429f8
yunicoin <90954972+yunicoin@users.noreply.github.com>
2026-06-08 17:13:51 +0300

    Update plot.py

--- python/plot.py
+++ python/plot.py
@@ -1,19 +1,22 @@
 import sys
+import os
+from plotters.heat_equation_plotter import HeatEquationPlotter
+
+# Добавляем папку, в которой лежит этот скрипт, в путь поиска модулей
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
 if __name__ == '__main__':
-
     if len(sys.argv) < 4:
         print('Usage: plot.py <plotter> <json_data_path> <output_path>')
         raise SystemError
 
     plotters = {
+        'heat_equation': HeatEquationPlotter,
     }
 
     if not (sys.argv[1] in plotters):
         raise SystemError
 
     Plotter = plotters[sys.argv[1]]
-
     plotter = Plotter(sys.argv[2], sys.argv[3])
-
     plotter.plot()
